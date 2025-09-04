f2a8b08a43fe8bb45644dfd050b3f73e57809778
Shivang Sharma <shivangs829@gmail.com>
2025-09-04 13:20:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Nth Fibonacci Number/nth-fibonacci-number.java
@@ -0,0 +1,17 @@
+// User function Template for Java
+
+class Solution {
+    public int nthFibonacci(int n) {
+        // code here
+        if(n==0){
+            return 0;
+        }
+        if(n==1){
+            return 1;
+        }
+        return nthFibonacci(n-1)+nthFibonacci(n-2);
+    }
+    public static void main(String args[]){
+        int n;
+    }
+}
